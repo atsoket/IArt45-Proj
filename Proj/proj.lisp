68f1ffffcf45ed82288e8844f3287889e951f712
atsoket <danielcostacct@gmail.com>
2015-10-18 19:20:01 +0000

    Foram adicionados comentarios

--- Proj/proj.lisp
+++ Proj/proj.lisp
@@ -15,9 +15,14 @@
 (defstruct tabuleiro
     (tabuleiro))
 
+;;Cria um tabuleiro de 17 linhas e 10 colunas.
 (defun cria-tabuleiro ()
     (make-array '(18 10))
 )
+
+;;Esta funcao, cria um tabuleiro auxiliar, e conforme percorre o 
+;;tabuleiro_original, copia os valores para o auxiliar.
+;;No fim retorna o tabuleiro auxiliar.
 
 (defun copia-tabuleiro (taboriginal)
     (let ( (tabnovo (make-array '(18 10)) ) )
@@ -29,19 +34,24 @@
     tabnovo)
 )
 
+;;Esta funcao verifica se uma dada posicao de um tabuleiro
+;; esta' preenchida.
 (defun tabuleiro-preenchido-p (tab l c)
     (cond ((equal (aref tab l c) NIL) NIL)
     (T T))
 )
 
+;;Percorre uma dada coluna, e retorna a altura assim que encontra uma casa preenchida.
 (defun tabuleiro-altura-coluna (tab c)
     (let ((altura 18) (contador 0))
         (loop while (< contador 18)  do
-            (if  (equal (aref tab contador c) NIL) (progn (decf altura) (incf contador)) (setf contador 10)) ;(setf contador 18)
+            (if  (equal (aref tab contador c) NIL) (progn (decf altura) (incf contador)) (setf contador 18)) ;(setf contador 18)
         )
     altura)
 )
 
+;;esta funcao retorna NIL, assim que encontrar uma posicao vazia.
+;;consequentemente interrompe o ciclo, visto que deixa de ser necessa'rio.
 (defun tabuleiro-linha-completa-p (tab l)
    (let ((contador 0) (completo T))
         (loop while (< contador 10)  do
@@ -50,10 +60,15 @@
     completo)
 )
 
+;;preenche uma posicao do tabuleiro.
+;;NOTA: Coloquei True, pk ainda nao decidimos que valor vai ser usado.
 (defun tabuleiro-preenche! (tab l c)
     (setf (aref tab l c) T)
 )
 
+
+;;escreve os valores da linha imediatamente acima na linha que tem de ser removida.
+;;NOTA: A linha do topo e' a zero, e a mais abaixo e' a 17.
 (defun tabuleiro-remove-linha! (tabOriginal linhaOriginal) 
        
     (let ((linhaApagar linhaOriginal) (linhaACopiar (- linhaOriginal 1)) )
@@ -70,15 +85,18 @@
     )
 )
 
-;;MELHORAR
+;;Nesta funcao, conforme e' encontrado uma posicao preenchida, retorna True.
 (defun tabuleiro-topo-preenchido-p (tab)
    (let ((contador 0) (completo NIL))
         (loop while (< contador 10)  do
-            (if  (equal (aref tab 0 contador) NIL) (incf contador) (progn (setf contador 10) (setf completo T))) ; O topo não é a linha 17?
+            (if  (equal (aref tab 0 contador) NIL) (incf contador) (progn (setf contador 10) (setf completo T)))
         )
     completo)
 )
 
+
+;;Esta fucnao, compara posicao a posicao dois tabuleiros,
+;;se todos os valores forem iguais, retorna True.
 (defun tabuleiros-iguais-p (tab1 tab2)
 
     (let ((iguais T))
@@ -90,6 +108,13 @@
     iguais)
 )
 
+;;DEVOLVE UM ELEMENTO RANDOM DA LISTA
+(defun random-element (list)
+  (nth (random (length list)) list))
+
+
+;;FUNCOES EXTRA, PARA AJUDAR A CRIAR TABULEIROS PREENCHIDOS.
+
 ;;ESTA FUNCAO PREENCHE O TBAULEIRO COM LIXO
 (defun bota-lixo (taboriginal)
         (dotimes (linha 18)
@@ -99,6 +124,11 @@
         )
 )
 
-;;DEVOLVE UM ELEMENTO RANDOM DA LISTA
-(defun random-element (list)
-  (nth (random (length list)) list))
+;;ESTA FUNCAO PREENCHE O TBAULEIRO COM LINHAS
+(defun preenche (taboriginal)
+        (dotimes (linha 18)
+            (dotimes (coluna 10)
+                (setf (aref taboriginal linha coluna) linha))
+            )
+        )
+
